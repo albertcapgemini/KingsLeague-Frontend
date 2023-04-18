b33317874ab7dcb92bdbf7eaac57477cb47e1cd8
Espina Megia <alberto.espina-megia@capgemini.com>
2023-04-18 12:06:41 +0200

    Cambios en edit

--- src/app/component/player-list/player-list/player-list.component.ts
+++ src/app/component/player-list/player-list/player-list.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Player } from 'src/app/model/player/player';
+import { PlayerService } from 'src/app/service/player/player.service';
 
 @Component({
   selector: 'app-player-list',
@@ -6,5 +9,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./player-list.component.scss']
 })
 export class PlayerListComponent {
+  player: Player[] = [];
 
+  constructor(private router: Router, private playerService: PlayerService){}
+
+  ngOnInit(): void {
+    this.playerService.getPlayer()
+      .subscribe(data => {
+        this.player = data;
+      });
+  };
+  deletePlayer(player: Player): void {
+    this.playerService.deletePlayer(player)
+      .subscribe(data => {
+        this.player = this.player.filter(u => u !== player);
+      })
+  };
+  editPlayer(player: Player): void {
+    this.playerService.editPlayer(player)
+      .subscribe(data => {
+        this.player = this.player
+      })
+  };
 }
